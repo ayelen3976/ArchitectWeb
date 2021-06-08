634be6fa3ce49f148eac864630c314cb285f978d
ayelen3976 <ayee_01@live.com>
2021-06-08 01:26:44 -0300

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavHome from '../Navbar/Navbar';
+
+function Home(){
+    return(
+        <div>
+       <NavHome/>
+        </div>
+    )
+}
+export default Home;
